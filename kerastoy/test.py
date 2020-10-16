99198839e5d238a0d30f0fed7b3d74c7e1fa29cc
Chen Cen <arrow.cen@gmail.com>
2020-10-16 01:21:13 -0700

    update test

--- kerastoy/test.py
+++ kerastoy/test.py
@@ -1,11 +1,24 @@
-import tensorflow as tf
-print("Num GPUs Available: ", len(tf.config.experimental.list_physical_devices('GPU')))
+import argparse
 
-tf.debugging.set_log_device_placement(True)
+# import tensorflow as tf
+# print("Num GPUs Available: ", len(tf.config.experimental.list_physical_devices('GPU')))
+#
+# tf.debugging.set_log_device_placement(True)
 
 # Create some tensors
-a = tf.constant([[1.0, 2.0, 3.0], [4.0, 5.0, 6.0]])
-b = tf.constant([[1.0, 2.0], [3.0, 4.0], [5.0, 6.0]])
-c = tf.matmul(a, b)
+# a = tf.constant([[1.0, 2.0, 3.0], [4.0, 5.0, 6.0]])
+# b = tf.constant([[1.0, 2.0], [3.0, 4.0], [5.0, 6.0]])
+# c = tf.matmul(a, b)
+# print(c)
 
-print(c)
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-blah", default='heyo')
+    args = parser.parse_args()
+    for _ in range(100000):
+        print(args.blah)
+
+
+if __name__ == "__main__":
+    main()
